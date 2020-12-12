bfe3ce642ed63496528fcc97caadeef658b8453e
Daniel Hermansen <daniel.j.hermansen@gmail.com>
2020-12-12 16:20:29 -0700

    MVP is working. Test sending back the expected response.

---
+++ ls8.py
@@ -0,0 +1,18 @@
+"""Main."""
+
+import sys
+from cpu import *
+
+cpu = CPU()
+
+try:
+    # filename = sys.argv[1]
+    filename = "sctest.ls8"
+    with open(filename) as f:
+        cpu.load(filename)
+        cpu.run()
+except IndexError:
+    print("please pass an input .ls8 file")
+except FileNotFoundError:
+    print("file not found.")
+
